953c27fa6cbedc8ea2c64ee80655a5c195f659da
kmots123 <kmots123@gmail.com>
2022-05-06 12:28:57 -0400

    added upgrade statement

--- fog-setup.sh
+++ fog-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # fog-setup.sh
 # pre-reqs
-apt-get update && apt-get install -y apache2 bc build-essential cpp curl g++ gawk gcc genisoimage git gzip htmldoc isolinux lftp libapache2-mod-php7.4 libc6 libcurl4 liblzma-dev m4 mariadb-client mariadb-server net-tools nfs-kernel-server openssh-server php7.4 php7.4-bcmath php7.4-cli php7.4-curl php7.4-fpm php7.4-gd php7.4-json php7.4-ldap php7.4-mbstring php7.4-mysql php7.4-mysqlnd tar tftp-hpa tftpd-hpa unzip vsftpd wget xinetd zlib1g && apt-get clean
+apt-get update && apt-get install -y apache2 bc build-essential cpp curl g++ gawk gcc genisoimage git gzip htmldoc isolinux lftp libapache2-mod-php7.4 libc6 libcurl4 liblzma-dev m4 mariadb-client mariadb-server net-tools nfs-kernel-server openssh-server php7.4 php7.4-bcmath php7.4-cli php7.4-curl php7.4-fpm php7.4-gd php7.4-json php7.4-ldap php7.4-mbstring php7.4-mysql php7.4-mysqlnd tar tftp-hpa tftpd-hpa unzip vsftpd wget xinetd zlib1g && apt-get upgrade -y && apt-get clean
 cd $HOME
 
 # download project
